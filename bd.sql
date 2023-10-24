62f2b95a13a8b2b8f6ac98eae7f63d8ab18230df
javierv318 <vasquezpabon2@gmail.com>
2023-10-24 01:31:37 -0500

    Proyecto funcional

---
+++ bd.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA `parcial3` ;
+
+CREATE TABLE `parcial3`.`datos_criadero` (
+  `timestamp` INT(12) NOT NULL,
+  `sensorId` INT(2) NOT NULL,
+  `temperature` FLOAT NOT NULL,
+  `humidity` FLOAT NOT NULL,
+  `thermalSensation` FLOAT NOT NULL,
+  `criadero` TINYINT NOT NULL,
+  PRIMARY KEY (`timestamp`));
