18d42aac94927ac6a9fa7672c6b47b44a36768f4
javierv318 <vasquezpabon2@gmail.com>
2023-11-19 18:27:54 -0500

    Duplicated dashboard main

--- frontend/src/routes/Dashboard2.tsx
+++ frontend/src/routes/Dashboard2.tsx
@@ -5,16 +5,14 @@ import {
   Table,
   Button,
   Container,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  FormGroup,
-  ModalFooter,
 } from "reactstrap";
+import { useAuth } from "../auth/AuthProvider";
+
+const auth = useAuth();
 
 var newData = new Array();
 function data() {
-  fetch("http://20.42.107.153:3000/parqueadero/camara", {
+  fetch("http://localhost:3000/datos/"+auth.user.nodo, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -32,29 +30,23 @@ function data() {
 }
 data();
 
-var vehiculo = new Array();
-
   class App extends React.Component {
     state = {
-      modalInsertar: false,
       data:newData,
+      modalInsertar: false,
       form: {
         id: "",
-        fechahora: "",
-        placa: ""
-      },
-      dataVehiculo:vehiculo,
-      formVehiculos:{
-        nombre_titular:"",
-        apellido_titular:"",
-        color_vehiculo:"",
-        marcha_vehiculo:"",
-        placa_vechiulo:""
+        timestamp:"",
+        sensorId: "",
+        temperature: "",
+        humidity: "",
+        thermalSensation: "",
+        criadero: ""
       }
     };
-
+    
     actualizar() {
-      fetch("http://20.42.107.153:3000/camara", {
+      fetch("http://localhost:3000/datos", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -75,84 +67,7 @@ var vehiculo = new Array();
           throw error;
         });
     }
-
-    agregarVehiculo = (datos:any) => {
-      var enviar = JSON.stringify(datos);
-      console.log(enviar);
-      return fetch('http://20.42.107.153:3000/parqueadero/camara/2', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: enviar
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('HTTP error! Status: ${response.status}');
-          }
-          console.log(response.status);
-          var exito = window.alert("Vehiculo insertado con éxito");
-          exito;
-        })
-        .catch((error) => {
-          throw error;
-        })
-    }
-
-    modalInsertar = () => {
-      this.setState({
-        modalInsertar: true,
-        formVehiculos:{
-          ...this.state.formVehiculos
-        }
-      });
-    }
-
-    cerrarModalInsertar = () => {
-      this.setState({modalInsertar:false})
-    }
-
-    verificar(dato:any) {
-      var ulr = "http://20.42.107.153:3000/parqueadero/camara/"+dato.placa;
-      fetch(ulr, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        }
-      })
-        .then((response) => {
-          if(response.status == 200){
-            var ver = window.alert("Verificado con éxito");
-            ver;
-          }
-          else{
-            var ver = window.alert("No se ha podido verificar");
-            ver;
-          }
-        })
-        .catch((error) => {
-          throw error;
-        });
-    }
     
-    handleChange = (e: { target: { name: any; value: any; }; }) => {
-      this.setState({
-        form: {
-          ...this.state.form,
-          [e.target.name]: e.target.value,
-        },
-      });
-    };
-
-    handleChangeVehiculo = (e: { target: { name: any; value: any; }; }) => {
-      this.setState({
-        formVehiculos: {
-          ...this.state.formVehiculos,
-          [e.target.name]: e.target.value,
-        },
-      });
-    };
-
     timestamp_to_iso_date(timestamp: any) {
 
       var date = new Date(0);
@@ -165,21 +80,33 @@ var vehiculo = new Array();
       return dayComplete;
     };
 
+    handleChange = (e: { target: { name: any; value: any; }; }) => {
+      this.setState({
+        form: {
+          ...this.state.form,
+          [e.target.name]: e.target.value,
+        },
+      });
+    };
+
     render() {
       
       return (
         <>
           <Container>
             <br />
-            <Button color="primary" onClick={()=>this.actualizar()}>Actualizar</Button>     <Button color="success" onClick={()=>this.modalInsertar()}>Agregar Vehiculo</Button>
+            <Button color="success" onClick={()=>this.actualizar()}>Actualizar</Button>
             <br />
             <Table>
               <thead>
                 <tr>
                   <th>Id</th>
-                  <th>Fecha y Hora</th>
-                  <th>Placa</th>
-                  <th>¿Verificado?</th>
+                  <th>Fecha y hora</th>
+                  <th>Nodo</th>
+                  <th>Temperatura</th>
+                  <th>Humedad</th>
+                  <th>Sensación termica</th>
+                  <th>¿Criadero?</th>
                 </tr>
               </thead>
               <tbody>
@@ -187,79 +114,19 @@ var vehiculo = new Array();
                   <tr key={dato.id}>
                     <td>{dato.id}</td>
                     <td>{this.timestamp_to_iso_date(dato.fechahora)}</td>
-                    <td>{dato.placa}</td>
-                    <td><Button color="info" onClick={() => {this.verificar(dato);}}>Verificar</Button></td>
+                    <td>{dato.sensorId}</td>
+                    <td>{dato.temperature}</td>
+                    <td>{dato.humidity}</td>
+                    <td>{dato.thermalSensation}</td>
+                    <td>{dato.criadero}</td>
                   </tr>
                 ))}
               </tbody>
             </Table>
+            <br />
           </Container>
-
-          <Modal isOpen={this.state.modalInsertar}>
-            <ModalHeader>
-              <div><h3>Agregar vehiculo</h3></div>
-            </ModalHeader>
-            <ModalBody>
-              <FormGroup>
-                <label>Nombre:</label>
-                <input
-                  className="form-control"
-                  name="nombre_titular"
-                  type="text"
-                  onChange={this.handleChangeVehiculo}
-                />
-              </FormGroup>
-              <FormGroup>
-                <label>Apellido:</label>
-                <input
-                  className="form-control"
-                  name="apellido_titular"
-                  type="text"
-                  onChange={this.handleChangeVehiculo}
-                />
-              </FormGroup>
-              <FormGroup>
-                <label>Marca</label>
-                <input
-                  className="form-control"
-                  name="marcha_vehiculo"
-                  type="text"
-                  onChange={this.handleChangeVehiculo}
-                />
-              </FormGroup>
-              <FormGroup>
-                <label>Color</label>
-                <input
-                  className="form-control"
-                  name="color_vehiculo"
-                  type="text"
-                  onChange={this.handleChangeVehiculo}
-                />
-              </FormGroup>
-              <FormGroup>
-                <label>Placa</label>
-                <input
-                  className="form-control"
-                  name="placa_vechiulo"
-                  type="text"
-                  onChange={this.handleChangeVehiculo}
-                />
-              </FormGroup>
-            </ModalBody>
-            <ModalFooter>
-              <Button
-                color='primary'
-                onClick={() => {this.agregarVehiculo(this.state.formVehiculos);this.setState({modalInsertar:false})}}
-              >Insertar</Button>
-              <Button
-                className='danger'
-                onClick={() => {this.setState({modalInsertar:false})}}
-              >Cancel</Button>
-            </ModalFooter>
-          </Modal>
         </>
       );
     }
   }
   export default App;
-  
